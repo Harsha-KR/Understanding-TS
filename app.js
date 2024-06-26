c7b98a2237aad3c07f9db0f6691b64554e3c76fe
Harsha-Kr1 <harsha.kr1@hotmail.com>
2024-06-26 08:42:32 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+function add(n1, n2) {
+    return n1 + n2;
+}
+var n1 = '5';
+var n2 = 2.8;
+var sum = add(n1, n2);
+console.log("The sum is : " + sum);
---
+++ app.ts
@@ -0,0 +1,10 @@
+function add(n1: number, n2: number){
+    return n1 + n2;
+}
+
+const n1 = 5;
+const n2 = 2.8;
+
+const sum = add(n1, n2);
+
+console.log("The sum is : " + sum);
