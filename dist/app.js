b14251a01659aebb697c23ef995d30767365f546
Harsha-Kr1 <harsha.kr1@hotmail.com>
2024-06-26 23:21:55 +0530

    Updated tsconfig file

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,8 @@
+"use strict";
 function add(n1, n2) {
     return n1 + n2;
 }
-var n1 = '5';
-var n2 = 2.8;
-var sum = add(n1, n2);
+const n1 = 5;
+const n2 = 2.8;
+const sum = add(n1, n2);
 console.log("The sum is : " + sum);
